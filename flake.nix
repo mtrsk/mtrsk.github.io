c9de75a5fd59be4c3f6c23417bc405d8e71a9da0
Marcos Benevides <marcos.schonfinkel@gmail.com>
2024-07-04 23:53:41 -0300

    switch to hugo + ox-hugo (#10)
    * switch to hugo + ox-hugo

--- flake.nix
+++ flake.nix
@@ -2,57 +2,62 @@
   description = "Blog";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, utils }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs)
-          dotnet-sdk
-          emacs-nox
-          emacsPackagesFor
-          gnumake
-          stdenv
-          mkShell
-          ;
-
-        customEmacs = (emacsPackagesFor emacs-nox).emacsWithPackages
-          (epkgs: with epkgs.melpaPackages; [
-            citeproc
-            ox-rss
-            evil
-            evil-collection
-            htmlize
-          ]
-          ++ (with epkgs.elpaPackages; [
-            org
-          ]));
-      in
-      {
-        packages.default = stdenv.mkDerivation {
-          name = "blog";
-          src = ./.;
-          buildInputs = [ customEmacs dotnet-sdk ];
-
-          buildPhase = ''
-            make publish
-          '';
-
-          installPhase = ''
-            mkdir -p $out
-            cp -r public/* $out
-          '';
+  outputs = { self, nixpkgs, devenv, ... } @ inputs:
+    let
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = f: builtins.listToAttrs (map (name: { inherit name; value = f name; }) systems);
+      mkPkgs = system: import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
         };
-
-        devShells.default = mkShell {
-          buildInputs = [
-            customEmacs
-            gnumake
-            dotnet-sdk
+      };
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = mkPkgs system;
+          
+          tooling = with pkgs; [
+            just
+            hugo
           ];
-        };
-      });
+          
+          customEmacs = (pkgs.emacsPackagesFor pkgs.emacs-nox).emacsWithPackages (epkgs:
+            with epkgs.melpaPackages; [
+              ox-hugo
+              citeproc
+            ] ++ (with epkgs.elpaPackages; [
+              org
+            ]));
+        in
+        {
+          # `nix develop .#ci`
+          # reduce the number of packages to the bare minimum needed for CI
+          ci = pkgs.mkShell {
+            buildInputs = [ customEmacs ] ++ tooling;
+          };
+
+          # `nix develop`
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              ({ pkgs, lib, ... }: {
+                packages = [ customEmacs ] ++ tooling;
+
+                enterShell = ''
+                  echo "Starting environment..."
+                '';
+              })
+            ];
+          };
+        });
+    };
 }
---
+++ justfile
@@ -0,0 +1,36 @@
+# Define source directory
+BLOG_SRC := 'content-org'
+BLOG_BUNDLE := 'site.tar.gz'
+
+# Define source files using wildcard
+SOURCES := `echo {{ BLOG_SRC }}/*.org`
+
+default:
+  just --list
+
+# Generate Markdown files
+content:
+    emacs $(pwd) --batch --load export.el
+
+# Publish content
+public: content
+    hugo
+
+# Run Hugo server with drafts
+run:
+	hugo server --buildDrafts --buildFuture
+
+# Build for CI
+ci-build: clean public
+
+# Removes org backups
+remove-org:
+    #!/usr/bin/env bash
+    find . -iname "#*.org#" | xargs rm -r 
+    echo "Finished!"
+
+# Cleans the current environment
+clean: remove-org
+	rm -rf content
+	rm -rf public
+	rm -rf {{ BLOG_BUNDLE }}
