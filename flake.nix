74ed50b279e7040d8386576547774f1706478022
Marcos Benevides <marcos.schonfinkel@gmail.com>
2024-07-06 11:45:02 -0300

    improve org-roam and note-taking experience

--- flake.nix
+++ flake.nix
@@ -29,6 +29,27 @@
             just
             hugo
           ];
+
+          texenv = pkgs.texlive.combine {
+            inherit (pkgs.texlive)
+              accsupp
+              collection-basic
+              collection-fontsextra
+              collection-fontsrecommended
+              collection-langenglish
+              collection-langportuguese
+              collection-latexextra
+              collection-mathscience
+              extsizes
+              etoolbox
+              hyphen-portuguese
+              latexmk
+              paracol
+              pdfx
+              ragged2e
+              scheme-medium
+              ;
+          };
           
           customEmacs = (pkgs.emacsPackagesFor pkgs.emacs-nox).emacsWithPackages (epkgs:
             with epkgs.melpaPackages; [
@@ -40,17 +61,23 @@
         in
         {
           # `nix develop .#ci`
-          # reduce the number of packages to the bare minimum needed for CI
+          # Reduce the number of packages to the bare minimum needed for CI,
+          # by removing LaTeX and not using my own Emacs configuration, but
+          # a custom package with just enough tools to generate the markdown
+          # for org-roam.
           ci = pkgs.mkShell {
             buildInputs = [ customEmacs ] ++ tooling;
           };
 
-          # `nix develop`
+          # `nix develop --impure`
+          # This is the development shell, meant to be used as an impure
+          # shell, so no custom Emacs here, just use your global package
+          # switch back to the CI shell for builds.
           default = devenv.lib.mkShell {
             inherit inputs pkgs;
             modules = [
               ({ pkgs, lib, ... }: {
-                packages = [ customEmacs ] ++ tooling;
+                packages = [ texenv ] ++ tooling;
 
                 scripts = {
                   build.exec = "just build";
