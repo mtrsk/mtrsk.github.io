ef5a70b73fab8938b6b47257ac9e281dfb70eba3
Marcos Benevides <marcos.schonfinkel@gmail.com>
2024-07-10 22:44:43 -0300

    F# fix

--- patch-markdown-links.fsx
+++ patch-markdown-links.fsx
@@ -44,25 +44,50 @@ let matchMany (regex: Regex) content =
     |> Seq.map (fun m -> m.Groups)
     |> List.ofSeq
 
+let fileExists (path: FilePath) =
+    match path with
+    | FilePath p -> if Path.Exists p then Ok path else FileNotFound p |> Error
+
+let toGuid (s: string) =
+    let status, guid = Guid.TryParse s
+    if status then Ok guid else InvalidGuid s |> Error
+
 let readNote (path: FilePath) =
     result {
         match path with
         | FilePath p ->
             let content = File.ReadAllLines(p) |> String.concat "\n"
-            let! title = matchSingle titleRx content |> Result.map (fun r -> r.[1].Value)
-            let! noteId = matchSingle idRx content |> Result.map (fun r -> r.[1].Value |> Guid)
-            let refs =
+            let! title = 
+                matchSingle titleRx content |> Result.map (fun r -> r.[1].Value)
+            let! noteId =
+                matchSingle idRx content |> Result.map (fun r -> r.[1].Value) |> Result.bind toGuid
+            let! refs =
                 content
                 |> matchMany brokenLinkRx
-                |> List.map (fun g -> g.[1].Value |> Guid)
-                |> Set.ofList
-            return { Id = noteId; Title = title; References = refs; Path = path }
+                |> List.map (fun g -> g.[1].Value)
+                |> List.traverseResultM toGuid
+            return { Id = noteId; Title = title; References = refs |> Set.ofList; Path = path }
     }
+
+let buildMap (r: Result<Note, ScriptErrors> array) =
+    let rec loop (m: Map<Guid, Note>) i stop =
+        if i = stop then m
+        else
+            match r[i] with
+            | Ok n -> loop (Map.add n.Id n m) (i + 1) stop
+            | Error e -> 
+                printfn "%A" e
+                loop m (i + 1) stop
+    let l = Array.length r
+    if l = 0 then Map.empty
+    else loop Map.empty 0 (l - 1)
 
 printfn "Reading: %A" markdownNotesPath
 
 let notes =
     Directory.GetFiles(markdownNotesPath, "*.md")
-    |> Array.map (fun p -> readNote (FilePath p))
+    |> Array.Parallel.map (FilePath >> fileExists >> Result.bind readNote)
+    |> buildMap
+let withReferences = notes |> Map.filter (fun _ v -> Set.isEmpty v.References |> not)
 
 printfn "%A" notes
