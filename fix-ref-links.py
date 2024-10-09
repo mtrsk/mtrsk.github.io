7305f866cf9ffbb072a74a4f0eb864ae461c8742
Marcos Benevides <marcos.schonfinkel@gmail.com>
2024-10-08 23:26:14 -0300

    it kinda works

--- fix-ref-links.py
+++ fix-ref-links.py
@@ -15,20 +15,23 @@ with open(JSON_PATH.resolve()) as f:
 graph = {k["id"].replace("\"", ""):k["lnk"] for k in graph_list["nodes"]}
 
 def rewrite(lnk: str) -> str:
+    year = lnk[:4]
     filename = lnk[15:]
     title = filename.replace("_", " ").title()
     #return f"[{title}]({{< relref \"../notes/{filename}.md\" >}})"
-    return f"[{title}]" + "({{< relref " + f"../notes/{filename}.md" + " >}})"
+    #return f"[{title}]" + "({{< relref " + f"../notes/{filename}.md" + " >}})"
+    #return f"[{title}](../notes/{filename}.md)"
+    return f"[{title}](/notes/{year}/{filename}/)"
 
 def fix_links(path: pathlib.PosixPath) -> None:
-    for line in fileinput.input([path], inplace=False):
+    for line in fileinput.input([path], inplace=True):
         matches = re.findall(PATTERN, line)
         if len(matches) > 0:
             for item in matches:
                 if item in graph:
-                    print(rewrite(graph[item]))
+                    #print(rewrite(graph[item]))
                     line = re.sub(PATTERN, rewrite(graph[item]), line, count=1)
-        #sys.stdout.write(line)
+        sys.stdout.write(line)
 
 if __name__ == "__main__":
     list(map(fix_links, [n.resolve() for n in MARKDOWN_NOTES if n.is_file()]))
